7d2ae13dcb4629b09af9514cbd1cbfd2ed534889
thatisdrtruman <76672439+thatisdrtruman@users.noreply.github.com>
2022-04-26 00:23:51 +0100

    Update test4.py
    fixed the small glitch

--- test4.py
+++ test4.py
@@ -42,7 +42,7 @@ def EnglishToInteger(englishNumber: Str) -> int:
                     numlistd[1+unitmove]=tydict[wordlistd[2]]
                     if wordlistd[3] in unitdict:
                         numlistd[2+unitmove]=unitdict[wordlistd[3]]
-                    elif wordlistd[3] in tydict or wordlistd in tendict:
+                    elif wordlistd[3] in tydict or wordlistd[3] in tendict:
                         ValueErrorRaiser()
                 elif wordlistd[2] in tendict:
                     if not wordlistd[1] == largenumber:
@@ -79,7 +79,7 @@ def EnglishToInteger(englishNumber: Str) -> int:
                     wordlist.pop(0)
                     break
                 wordlist.pop(0)
-        except IndexError:
+        except (KeyError,IndexError, ValueError) as e:
             ValueErrorRaiser()
     if not type(englishNumber) is str or englishNumber.isnumeric() or englishNumber == "and" or englishNumber == "" or englishNumber.isspace():
         ValueErrorRaiser()
@@ -114,39 +114,41 @@ def EnglishToInteger(englishNumber: Str) -> int:
     k=len(wordlist)
     if k > 4:
         ValueErrorRaiser()
-    if "hundred" in wordlist:
-        if wordlist[0] not in unitdict:
-            ValueErrorRaiser()
-        numlist[12]=unitdict[wordlist[0]]
-        if k == 4: #if the numlist is in 4 words: [hundred number], ['hundred'], [ty number], [unit number]
-            if wordlist[2] in tydict:
-                numlist[13]=tydict[wordlist[2]]
-                numlist[14]=unitdict[wordlist[3]]
-            elif wordlist[2] not in tydict and wordlist[3] in goodword:
+    try:
+        if "hundred" in wordlist:
+            if wordlist[0] not in unitdict:
                 ValueErrorRaiser()
-        elif k == 3: # if numlist is [hundred number], hundred, [unit number/ ty number/ ten number]
-            if wordlist[2] in tydict:
-                numlist[13]=tydict[wordlist[2]]
-            elif wordlist[2] in tendict:
+            numlist[12]=unitdict[wordlist[0]]
+            if k == 4: #if the numlist is in 4 words: [hundred number], ['hundred'], [ty number], [unit number]
+                if wordlist[2] in tydict:
+                    numlist[13]=tydict[wordlist[2]]
+                    numlist[14]=unitdict[wordlist[3]]
+                else:
+                    ValueErrorRaiser()
+            elif k == 3: # if numlist is [hundred number], hundred, [unit number/ ty number/ ten number]
+                if wordlist[2] in tydict:
+                    numlist[13]=tydict[wordlist[2]]
+                elif wordlist[2] in tendict:
+                    numlist[13]=1
+                    numlist[14]=tendict[wordlist[2]]
+                elif wordlist[2] in unitdict:
+                    numlist[14]=unitdict[wordlist[2]]
+        elif k == 2: #if numlist is [ty number], [unit number]
+            if wordlist[0] in tydict:
+                numlist[13]=tydict[wordlist[0]]
+                numlist[14]=unitdict[wordlist[1]]
+            else:
+                ValueErrorRaiser()
+        elif k == 1:
+            if wordlist[0] in tydict:
+                numlist[13]=tydict[wordlist[0]]
+            if wordlist[0] in tendict:
                 numlist[13]=1
-                numlist[14]=tendict[wordlist[2]]
-            elif wordlist[2] in unitdict:
-                numlist[14]=unitdict[wordlist[2]]
-    elif k == 2: #if numlist is [ty number], [unit number]
-        if wordlist[0] in tydict:
-            numlist[13]=tydict[wordlist[0]]
-            numlist[14]=unitdict[wordlist[1]]
-        else:
-            ValueErrorRaiser()
-    elif k == 1:
-        if wordlist[0] in tydict:
-            numlist[13]=tydict[wordlist[0]]
-        if wordlist[0] in tendict:
-            numlist[13]=1
-            numlist[14]=tendict[wordlist[0]]
-        if wordlist[0] in unitdict:
-            numlist[14]=unitdict[wordlist[0]]
-
+                numlist[14]=tendict[wordlist[0]]
+            if wordlist[0] in unitdict:
+                numlist[14]=unitdict[wordlist[0]]
+    except (KeyError, ValueError, IndexError) as e:
+        ValueErrorRaiser()
     while True:
         try:
             if numlist[0] == 0:
@@ -224,4 +226,111 @@ EnglishToInteger("eight hundred thousand two hundred and thirteen")
 EnglishToInteger("eight hundred thousand two hundred and thirty")
 EnglishToInteger("eight hundred thousand two hundred and thirty-four")
 
-assert EnglishToInteger("one twenty-forty") == ValueError
+EnglishToInteger("two million")
+
+EnglishToInteger("two million and one")
+EnglishToInteger("two million and ten")
+EnglishToInteger("two million and fifty")
+EnglishToInteger("two million and sixty-two")
+EnglishToInteger("two million and six hundred")
+EnglishToInteger("two million and six hundred and two")
+EnglishToInteger("two million and six hundred and twelve")
+EnglishToInteger("two million and six hundred and fifty")
+EnglishToInteger("two million and six hundred and fifty-four")
+EnglishToInteger("two million one thousand")
+EnglishToInteger("two million one thousand and one")
+EnglishToInteger("two million one thousand and sixteen")
+EnglishToInteger("two million one thousand and sixty")
+EnglishToInteger("two million one thousand and sixty-three")
+EnglishToInteger("two million one thousand, two hundred")
+EnglishToInteger("two million one thousand two hundred and one")
+EnglishToInteger("two million one thousand two hundred and thirteen")
+EnglishToInteger("two million one thousand two hundred and thirty")
+EnglishToInteger("two million one thousand two hundred and thirty-four")
+EnglishToInteger("two million ten thousand")
+EnglishToInteger("two million eleven thousand and one")
+EnglishToInteger("two million eleven thousand and sixteen")
+EnglishToInteger("two million ten thousand and sixty")
+EnglishToInteger("two million ten thousand and sixty-three")
+EnglishToInteger("two million ten thousand, two hundred")
+EnglishToInteger("two million ten thousand two hundred and one")
+EnglishToInteger("two million ten thousand two hundred and thirteen")
+EnglishToInteger("two million ten thousand two hundred and thirty")
+EnglishToInteger("two million ten thousand two hundred and thirty-four")
+EnglishToInteger("two million fifty thousand")
+EnglishToInteger("two million fifty thousand and one")
+EnglishToInteger("two million fifty thousand and sixteen")
+EnglishToInteger("two million fifty thousand and sixty")
+EnglishToInteger("two million fifty thousand and sixty-three")
+EnglishToInteger("two million fifty thousand, two hundred")
+EnglishToInteger("two million fifty thousand two hundred and one")
+EnglishToInteger("two million fifty thousand two hundred and thirteen")
+EnglishToInteger("two million fifty thousand two hundred and thirty")
+EnglishToInteger("two million thirty thousand two hundred and thirty-four")
+
+EnglishToInteger("two million fifty-one thousand")
+EnglishToInteger("two million fifty-one thousand and one")
+EnglishToInteger("two million fifty-one thousand and sixteen")
+EnglishToInteger("two million fifty-one thousand and sixty")
+EnglishToInteger("two million fifty-one thousand and sixty-three")
+EnglishToInteger("two million fifty-one thousand, two hundred")
+EnglishToInteger("two million fifty-one thousand two hundred and one")
+EnglishToInteger("two million fifty-one thousand two hundred and thirteen")
+EnglishToInteger("two million fifty-one thousand two hundred and thirty")
+EnglishToInteger("two million thirty-one thousand two hundred and thirty-four")
+
+EnglishToInteger("two million eight hundred thousand")
+EnglishToInteger("two million eight hundred thousand and one")
+EnglishToInteger("two million eight hundred thousand and sixteen")
+EnglishToInteger("two million eight hundred thousand and sixty")
+EnglishToInteger("two million eight hundred thousand and sixty-three")
+EnglishToInteger("two million eight hundred thousand, two hundred")
+EnglishToInteger("two million eight hundred thousand two hundred and one")
+EnglishToInteger("two million eight hundred thousand two hundred and thirteen")
+EnglishToInteger("two million eight hundred thousand two hundred and thirty")
+EnglishToInteger("two million eight hundred thousand two hundred and thirty-four")
+
+EnglishToInteger("two million eight hundred and six thousand")
+EnglishToInteger("two million eight hundred and six thousand and one")
+EnglishToInteger("two million eight hundred and six thousand and sixteen")
+EnglishToInteger("two million eight hundred and six thousand and sixty")
+EnglishToInteger("two million eight hundred and six thousand and sixty-three")
+EnglishToInteger("two million eight hundred and six thousand, two hundred")
+EnglishToInteger("two million eight hundred and six thousand two hundred and one")
+EnglishToInteger("two million eight hundred and six thousand two hundred and thirteen")
+EnglishToInteger("two million eight hundred and six thousand two hundred and thirty")
+EnglishToInteger("two million eight hundred and six thousand two hundred and thirty-four")
+
+EnglishToInteger("two million eight hundred and sixteen thousand")
+EnglishToInteger("two million eight hundred and sixteen thousand and one")
+EnglishToInteger("two million eight hundred and sixteen thousand and sixteen")
+EnglishToInteger("two million eight hundred and sixteen thousand and sixty")
+EnglishToInteger("two million eight hundred and sixteen thousand and sixty-three")
+EnglishToInteger("two million eight hundred and sixteen thousand, two hundred")
+EnglishToInteger("two million eight hundred and sixteen thousand two hundred and one")
+EnglishToInteger("two million eight hundred and sixteen thousand two hundred and thirteen")
+EnglishToInteger("two million eight hundred and sixteen thousand two hundred and thirty")
+EnglishToInteger("two million eight hundred and sixteen thousand two hundred and thirty-four")
+
+EnglishToInteger("two million eight hundred and ninety thousand")
+EnglishToInteger("two million eight hundred and ninety thousand and one")
+EnglishToInteger("two million eight hundred and ninety thousand and sixteen")
+EnglishToInteger("two million eight hundred and ninety thousand and sixty")
+EnglishToInteger("two million eight hundred and ninety thousand and sixty-three")
+EnglishToInteger("two million eight hundred and ninety thousand, two hundred")
+EnglishToInteger("two million eight hundred and ninety thousand two hundred and one")
+EnglishToInteger("two million eight hundred and ninety thousand two hundred and thirteen")
+EnglishToInteger("two million eight hundred and ninety thousand two hundred and thirty")
+EnglishToInteger("two million eight hundred and ninety thousand two hundred and thirty-four")
+
+EnglishToInteger("two million eight hundred and ninety-five thousand")
+EnglishToInteger("two million eight hundred and ninety-five thousand and one")
+EnglishToInteger("two million eight hundred and ninety-five thousand and sixteen")
+EnglishToInteger("two million eight hundred and ninety-five thousand and sixty")
+EnglishToInteger("two million eight hundred and ninety-five thousand and sixty-three")
+EnglishToInteger("two million eight hundred and ninety-five thousand, two hundred")
+EnglishToInteger("two million eight hundred and ninety-five thousand two hundred and one")
+EnglishToInteger("two million eight hundred and ninety-five thousand two hundred and thirteen")
+EnglishToInteger("two million eight hundred and ninety-five thousand two hundred and thirty")
+EnglishToInteger("two million eight hundred and ninety-five thousand two hundred and thirty-four")
+assert EnglishToInteger("one hundred ninety-eleven") == ValueError
